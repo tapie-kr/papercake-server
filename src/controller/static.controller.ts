2c9585893a2307f077ec83c75dcbded2623fce38
Yuchan Han <serltretu24@gmail.com>
2025-01-25 02:32:34 +0900

    Implement basic response for tracker.js endpoint in static controller

--- src/controller/static.controller.ts
+++ src/controller/static.controller.ts
@@ -3,6 +3,8 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 export default async function staticController(fastify: FastifyInstance) {
   fastify.get(
     "/static/:version/tracker.js",
-    async (req: FastifyRequest, res: FastifyReply) => {},
+    async (req: FastifyRequest, res: FastifyReply) => {
+      return "Hello World";
+    },
   );
 }
