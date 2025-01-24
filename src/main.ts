aad63c769bd1c970d98a6c9d56029f59cadcacd2
Yuchan Han <serltretu24@gmail.com>
2025-01-25 02:32:28 +0900

    Refactor server initialization to use createApp function for improved clarity

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import app from "./app";
+import createApp from "./app";
 
 const SERVER_PORT = Number(process.env.SERVER_PORT) || 443;
 
 async function bootstrap() {
+  const server = await createApp();
+
   try {
-    await app.listen({ port: SERVER_PORT, host: "0.0.0.0" });
+    await server.listen({ port: SERVER_PORT, host: "0.0.0.0" });
     console.log("Listening on port " + SERVER_PORT);
   } catch (err) {
-    app.log.error(`Server Error: ${err}`);
+    server.log.error(`Server Error: ${err}`);
     process.exit(1);
   }
 }
