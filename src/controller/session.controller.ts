b2b7151fc6cb8157f9da2573da7513f280279269
Yuchan Han <serltretu24@gmail.com>
2025-01-25 15:42:31 +0900

    Update session route to support server and filename parameters

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -2,20 +2,20 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { getCurrentHostURI, isValidProjectId } from "@/common/utils";
 import { browserClient, SiteType } from "@/common/request";
 
-// https://localhost/session/c.gif?server=c
+// https://localhost/session/c/c.gif
 
 export default async function sessionController(fastify: FastifyInstance) {
   fastify.get(
-    "/session/:filename.gif",
+    "/session/:server/:filename.gif",
     async (
-      request: FastifyRequest<{ Params: { server: string } }>,
+      request: FastifyRequest<{ Params: { server: string; filename: string } }>,
       reply: FastifyReply,
     ) => {
-      const { server } = request.params;
-      if (server.length > 2) {
+      const { server, filename } = request.params;
+      if (server.length > 2 || filename.length > 2) {
         reply.send(
           request.server.httpErrors.badRequest(
-            "This server ID is not allowed.",
+            "This server ID or filename is not allowed.",
           ),
         );
         return;
