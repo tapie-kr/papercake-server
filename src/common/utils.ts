a11ec9441a71b84e13e0e0f9fb8df61197e43a56
Yuchan Han <serltretu24@gmail.com>
2025-01-25 14:29:12 +0900

    Add utility function for parsing and validating project IDs from environment variables

---
+++ src/common/utils.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+
+function parseENVList(input: string): string[] {
+  const valueString = input.split("=")[1]?.trim();
+  if (!valueString) return [];
+
+  return valueString.split(",").map((id) => id.trim());
+}
+
+const ALLOW_PROJECT_ID = parseENVList(
+  process.env["ALLOW_PROJECT_ID"] as string,
+);
+
+function isValidProjectId(projectId: string): boolean {
+  return ALLOW_PROJECT_ID.includes(projectId);
+}
