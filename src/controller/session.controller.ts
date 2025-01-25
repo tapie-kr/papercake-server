65767c7efef714b538c61290c6328cb50880d455
Yuchan Han <serltretu24@gmail.com>
2025-01-25 16:27:40 +0900

    Implement first and second step session handling logic

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { getCurrentHostURI, isValidProjectId } from "@/common/utils";
 import { browserClient, SiteType } from "@/common/request";
+import { SessionSecondStepQuery } from "@/common/variable";
 
 // https://localhost/session/c/c.gif
 
@@ -8,10 +9,15 @@ export default async function sessionController(fastify: FastifyInstance) {
   fastify.get(
     "/session/:server/:filename.gif",
     async (
-      request: FastifyRequest<{ Params: { server: string; filename: string } }>,
+      request: FastifyRequest<{
+        Params: { server: string; filename: string };
+        Querystring: Record<string, string>;
+      }>,
       reply: FastifyReply,
     ) => {
+      const hostURI = getCurrentHostURI(request);
       const { server, filename } = request.params;
+
       if (server.length > 2 || filename.length > 2) {
         reply.send(
           request.server.httpErrors.badRequest(
@@ -19,6 +25,41 @@ export default async function sessionController(fastify: FastifyInstance) {
           ),
         );
         return;
+      }
+      console.log("keylength", Object.keys(request.query).length);
+      console.log("keys", request.query);
+      if (Object.keys(request.query).length === 0) {
+        const instance = await browserClient.create(SiteType.SESSION_CREATE);
+        const response = await instance.get(
+          `https://${server}.clarity.ms/${filename}.gif`,
+        );
+        if (response.status != 302) {
+          reply.send(
+            request.server.httpErrors.internalServerError(
+              "Response from an invalid server.",
+            ),
+          );
+          return;
+        }
+        const redirectURI = response.headers["location"] as string;
+        const convertedURI = redirectURI.replace(
+          /https:\/\/c\.bing\.com\/([^"?]+)\.gif(\?[^"]*)/g,
+          `${hostURI}/session/c/$1.gif$2`,
+        );
+        console.log(response.headers);
+        const headers = Object.entries(response.headers).reduce(
+          (acc, [key, value]) => {
+            if (key !== "location") {
+              acc[key] = value;
+            }
+            return acc;
+          },
+          {} as Record<string, string | string[]>,
+        );
+        reply.headers(headers);
+        reply.redirect(convertedURI, 302);
+        return;
+      } else if (SessionSecondStepQuery.every((key) => key in request.query)) {
       }
     },
   );
